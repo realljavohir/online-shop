7efd593585efaafb2ed8ef2c125cee5d9ac33dc9
Javohir <kingtheworld39@gmail.com>
2026-03-29 09:40:54 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Bot tokenini @BotFather dan oling
+BOT_TOKEN = "8753597096:AAFNeqOiDKYzatgrcuiQn22c0tNAhc5jTs0"
+
+# Adminlar Telegram ID lari ro'yxati
+ADMIN_IDS = [6713905538]  # O'zingizning ID laringizni qo'ying
+
+# PostgreSQL uchun (agar ishlatsangiz)
+# DATABASE_URL = "postgresql://username:password@localhost/delivery_bot"
